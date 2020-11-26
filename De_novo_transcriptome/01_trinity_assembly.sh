1556853f4ae41a0c7eb78c4df61888c03b3b4aae
Hollie Marshall <hollie_marshall@hotmail.co.uk>
2020-11-26 12:57:24 +0000

    updated file names

--- De_novo_transcriptome/01_trinity_assembly.sh
+++ De_novo_transcriptome/01_trinity_assembly.sh
@@ -11,7 +11,7 @@ cd $SCRATCH
 
 start=`date +%s`
 
-# NOTE: conda activate /ceph/users/afilia/.conda/envs/afilia_trinity
+# NOTE: conda activate holl_trinity
 #---------------------------------------------
 
 echo "copying data in"
@@ -21,8 +21,8 @@ rsync /data/ross/mealybugs/analyses/hollie/sex-specific/expression/02_trimmed/*.
 
 echo "gettin them annotations"
 Trinity --seqType fq \
---left trim_PC_F1_1.fq.gz, trim_PC_F2_1.fq.gz, trim_PC_F3_1.fq.gz, trim_PC_M1_1.fq.gz, trim_PC_M2_1.fq.gz, trim_PC_M3_1.fq.gz \
---right trim_PC_F1_2.fq.gz, trim_PC_F2_2.fq.gz, trim_PC_F3_2.fq.gz, trim_PC_M1_2.fq.gz, trim_PC_M2_2.fq.gz, trim_PC_M3_2.fq.gz \
+--left trim_PC_F1_1.fq.gz,trim_PC_F2_1.fq.gz,trim_PC_F3_1.fq.gz,trim_PC_M1_1.fq.gz,trim_PC_M2_1.fq.gz,trim_PC_M3_1.fq.gz \
+--right trim_PC_F1_2.fq.gz,trim_PC_F2_2.fq.gz,trim_PC_F3_2.fq.gz,trim_PC_M1_2.fq.gz,trim_PC_M2_2.fq.gz,trim_PC_M3_2.fq.gz \
 --SS_lib_type RF --max_memory 100G --CPU 32 --full_cleanup
 
 #---------------------------------------------
