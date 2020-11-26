e94b3e6ebc3df384cf3cac1a16c9cc57ccfe732b
Hollie Marshall <hollie_marshall@hotmail.co.uk>
2020-11-26 12:33:24 +0000

    added sample names

--- De_novo_transcriptome/01_trinity_assembly.sh
+++ De_novo_transcriptome/01_trinity_assembly.sh
@@ -1,7 +1,7 @@
 #$ -V
 #$ -cwd
 #$ -j y
-#$ -o /data/ross/mealybugs/analyses/hollie/logs/XXX_$JOB_ID.o
+#$ -o /data/ross/mealybugs/analyses/hollie/logs/trinity_$JOB_ID.o
 
 set -e
 
@@ -11,26 +11,24 @@ cd $SCRATCH
 
 start=`date +%s`
 
-# NOTE: conda activate afilia_trinity
+# NOTE: conda activate /ceph/users/afilia/.conda/envs/afilia_trinity
 #---------------------------------------------
 
 echo "copying data in"
+rsync /data/ross/mealybugs/analyses/hollie/sex-specific/expression/02_trimmed/*.fq.gz ./
 
 #---------------------------------------------
 
-echo "doing the shiz"
-
-# TRIM READS IF NEEDED BEFORE THIS
-
-Trinity --seqType fq \ 
---left <read1> \
---right <read2> \
+echo "gettin them annotations"
+Trinity --seqType fq \
+--left trim_PC_F1_1.fq.gz, trim_PC_F2_1.fq.gz, trim_PC_F3_1.fq.gz, trim_PC_M1_1.fq.gz, trim_PC_M2_1.fq.gz, trim_PC_M3_1.fq.gz \
+--right trim_PC_F1_2.fq.gz, trim_PC_F2_2.fq.gz, trim_PC_F3_2.fq.gz, trim_PC_M1_2.fq.gz, trim_PC_M2_2.fq.gz, trim_PC_M3_2.fq.gz \
 --SS_lib_type RF --max_memory 100G --CPU 32 --full_cleanup
 
 #---------------------------------------------
 
 echo "moving outputs"
-mv ./*.txt /data/ross/mealybugs/analyses/hollie/sex-specific
+mv *.fasta /data/ross/mealybugs/analyses/hollie/sex-specific/expression/de_novo_transcriptome
 
 echo "a clean directory is a happy directory"
 rm -r $SCRATCH/*
